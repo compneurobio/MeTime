7fcf7785d2c53a32a2c6bc1e99cbd2c4f3c1fc85
Krishna Bharadwaj <krishnabharadwaj35@gmail.com>
2022-07-26 11:46:22 +0200

    added merge function to merge two or more metime_analyser objects

--- R/fun_mod.R
+++ R/fun_mod.R
@@ -193,3 +193,25 @@ setMethod("mod_filter_tp", "metime_analyser", function(object, timepoints, full=
   }
   return(object)
 })
+
+
+#' Function to merge one or more metime_analyser objects
+#' @description function to merge multiple metime_analyser objects
+#' @param list_of_objects list of metime analyser objects that are to be merged
+#' @param annotations_index new list with annotations_index. Can also set to be NULL.
+#' @returns A merged metime_analyser object
+#' @export
+setGeneric("mod_merge_metime_analysers", function(list_of_objects, annotations_index) standardGeneric("mod_merge_metime_analysers"))
+setMethod("mod_merge_metime_analysers", "metime_analyser", function(list_of_objects, annotations_index) {
+				list_of_data <- lapply(list_of_objects, function(x) return(x@list_of_data))
+				list_of_col_data <- lapply(list_of_objects, function(x) return(x@list_of_col_data))
+				list_of_row_data <- lapply(list_of_objects, function(x) return(x@list_of_row_data))
+				if(is.null(annotations_index)) {
+						final_object <- new("metime_analyser", list_of_data=list_of_data, 
+							list_of_row_data=list_of_row_data, list_of_col_data=list_of_col_data, annotations=NULL)
+				} else {
+						final_object <- new("metime_analyser", list_of_data=list_of_data, 
+							list_of_row_data=list_of_row_data, list_of_col_data=list_of_col_data, annotations=annotations_index)	
+				}
+				return(final_object)
+	}) 
